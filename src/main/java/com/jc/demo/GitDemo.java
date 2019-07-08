18566a68dd45f8d095ff49b6f6f1951d1582e900
YiDa <867256260@qq.com>
2019-07-08 16:46:42 +0800

    first change

---
+++ src/main/java/com/jc/demo/GitDemo.java
@@ -0,0 +1,13 @@
+package com.jc.demo;
+
+/**
+ * @Auther: 帅气的达
+ * @Date: 2019/7/8 16:42
+ * @Description:
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("我的第一个Git项目");
+
+    }
+}
